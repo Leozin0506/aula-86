91c4b8f780334c24490e9e24e35063ec353684d2
Leozin0506 <94076803+Leozin0506@users.noreply.github.com>
2023-03-03 10:34:47 -0300

    Add files via upload

---
+++ PRO_1-1_C86_AtividadeDoAluno1-main/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC71zsevOgbXbHfHiOXlSUo8K4mGIUiSc0",
+  authDomain: "narracaodehistorias-fa705.firebaseapp.com",
+  projectId: "narracaodehistorias-fa705",
+  storageBucket: "narracaodehistorias-fa705.appspot.com",
+  messagingSenderId: "687583043157",
+  appId: "1:687583043157:web:d93087b4441be038557edc"
+};
